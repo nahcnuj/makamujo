c8699cf4ebf4ee96b37370f46b1a53671099a82b
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-11-19 10:01:42 +0900

    Layouting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
+import { Box, Container, Layout } from "automated-gameplay-transmitter";
 import "./index.css";
 
 export function App() {
   return (
-    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
-      <h1 className="text-5xl font-bold my-4 leading-tight">
-        <ruby>馬<rp>(</rp><rt>ま</rt><rp>)</rp></ruby>
-        <ruby>可<rp>(</rp><rt>か</rt><rp>)</rp></ruby>
-        <ruby>無<rp>(</rp><rt>む</rt><rp>)</rp></ruby>
-        <ruby>序<rp>(</rp><rt>じょ</rt><rp>)</rp></ruby>
-      </h1>
-      <p>
-        MAKA Mujo, an AI-VTuber
-      </p>
-    </div>
+    <Layout count={10} span={8} className="bg-emerald-950/30 text-emerald-50 font-[Noto_Sans_CJK_JP] font-bold">
+      <Container>
+        <div className="text-xs opacity-25">
+          {new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })}
+        </div>
+      </Container>
+      <Container>
+        <div className="h-full flex flex-col justify-between items-center">
+          <div className="flex-none">
+            <div className="text-5xl/15 text-emerald-300" style={{ rubyPosition: 'under' }}>
+              <ruby>馬<rp>(</rp><rt>ま</rt><rp>)</rp></ruby>
+              <ruby>可<rp>(</rp><rt>か</rt><rp>)</rp></ruby>
+              <ruby>無<rp>(</rp><rt>む</rt><rp>)</rp></ruby>
+              <ruby>序<rp>(</rp><rt>じょ</rt><rp>)</rp></ruby>
+            </div>
+          </div>
+          <div className="flex-none">
+            <div className="my-1 text-center text-3xl/12 text-white">
+              <span className="px-3 bg-black rounded-sm">
+                <span className="pr-3 font-normal text-white">&#x1D54F;</span>
+                &#xFF20;
+                <span className="font-mono">makamujo</span>
+              </span>
+            </div>
+          </div>
+          <div className="flex-auto w-full">
+            <Box>
+              Side
+            </Box>
+          </div>
+        </div>
+      </Container>
+      <Container>
+        Sprite
+        <Box>
+          Bottom
+        </Box>
+      </Container>
+    </Layout>
   );
 }
 
