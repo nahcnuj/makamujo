8a88199a1ed23b891235c4a3fdb34f26e451bc04
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-11-19 09:48:30 +0900

    Init a Bun project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+      <h1 className="text-5xl font-bold my-4 leading-tight">
+        <ruby>馬<rp>(</rp><rt>ま</rt><rp>)</rp></ruby>
+        <ruby>可<rp>(</rp><rt>か</rt><rp>)</rp></ruby>
+        <ruby>無<rp>(</rp><rt>む</rt><rp>)</rp></ruby>
+        <ruby>序<rp>(</rp><rt>じょ</rt><rp>)</rp></ruby>
+      </h1>
+      <p>
+        MAKA Mujo, an AI-VTuber
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/frontend.tsx
@@ -0,0 +1,21 @@
+/**
+ * This file is the entry point for the React app, it sets up the root
+ * element and renders the App component to the DOM.
+ *
+ * It is included in `src/index.html`.
+ */
+
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+import { StrictMode } from "react";
+
+function start() {
+  const root = createRoot(document.getElementById("root")!);
+  root.render(<StrictMode><App /></StrictMode>);
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", start);
+} else {
+  start();
+}
