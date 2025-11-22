d2fc1d17e604a9945cf80073dfb1210a17932b12
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-11-22 18:58:18 +0900

    Fix

--- lib/Agent/games/org.dashnet.orteil/cookieclicker/solver.ts
+++ lib/Agent/games/org.dashnet.orteil/cookieclicker/solver.ts
@@ -4,24 +4,28 @@ import type { Action, State } from "../../../../Browser/socket";
 let waiting: Action | undefined;
 
 export const solve = (s: State): Action => {
+  console.debug('[DEBUG]', 'solve', s);
+
+  if (s.name === 'clicked') {
+    waiting = undefined;
+    return {
+      name: 'noop',
+    };
+  }
+
   if (waiting) {
     console.debug('[DEBUG]', 'waiting action', waiting);
     return {
       name: 'noop',
     };
   }
-  console.debug('[DEBUG]', 'solve', s);
+
   if (s.name === 'opened') {
     return waiting = {
       name: 'click',
       target: '日本語', // TODO
       id: randomUUID(),
     } satisfies Action;
-  } else if (s.name === 'clicked') {
-    waiting = undefined;
-    return {
-      name: 'noop',
-    };
   } else if (s.name === 'idle') {
     return {
       name: 'noop',
