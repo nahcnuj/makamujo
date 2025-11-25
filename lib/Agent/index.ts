9c0df9cb83efb7e877345a7bea0d291128f5e430
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-11-25 20:43:02 +0900

    Fix

--- lib/Agent/index.ts
+++ lib/Agent/index.ts
@@ -63,6 +63,8 @@ export class MakaMujo {
         ...this.#speechListeners.map(f => f(text)),
       ]);
     }).catch(() => Promise.resolve());
+
+    await this.#speechPromise;
   }
 
   onSpeech(cb: (text: string) => Promise<void>): MakaMujo {
