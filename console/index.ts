a362a75fb96dcf30aa4d5dd58adbb4f686c6e958
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-12-08 21:37:16 +0900

    Debug

--- console/index.ts
+++ console/index.ts
@@ -4,18 +4,21 @@ import index from "./src/index.html";
 const server = serve({
   routes: {
     // Serve index.html for all unmatched routes.
-    '/*': index,
+    // '/*': index,
+    '/*': new Response('Hello\n', { headers: { 'Cache-Control': 'no-store' } }),
 
     '/robots.txt': new Response('User-agent: *\nDisallow: /\n'),
 
     "/api/hello": {
       async GET(req) {
+        console.debug(req.text());
         return Response.json({
           message: "Hello, world!",
           method: "GET",
         });
       },
       async PUT(req) {
+        console.debug(req.text());
         return Response.json({
           message: "Hello, world!",
           method: "PUT",
@@ -24,6 +27,7 @@ const server = serve({
     },
 
     "/api/hello/:name": async req => {
+      console.debug(req.text());
       const name = req.params.name;
       return Response.json({
         message: `Hello, ${name}!`,
