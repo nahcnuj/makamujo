edc4a59589fe29905660a3dbb531f94691e2082c
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2026-08-23 07:53:37 +0900

    fix
    https://github.com/honojs/hono/issues/5277

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -1,16 +1,18 @@
-/**
+﻿/**
  * This file is the entry point for the app, it sets up the root
  * element and renders the App component to the DOM.
  *
  * It is included in `src/index.html`.
  */
-import { render } from "hono/jsx/dom";
+import { createRoot } from "hono/jsx/dom/client";
 import { App } from "./App";
 
 function start() {
   const el = document.getElementById("root");
   if (!el) throw new Error("root element not found");
-  render(<App />, el);
+
+  const root = createRoot(el);
+  root.render(<App />);
 }
 
 if (document.readyState === "loading") {
